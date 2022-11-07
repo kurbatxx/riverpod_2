dede37ea5a8e4568a00bf172f0ad1b802ff7202a
kurbatov <kurbatxx@gmail.com>
2022-11-07 15:30:53 +0600

    first commit

---
+++ example3/lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+enum City {
+  stockholm,
+  paris,
+  tokyo,
+}
+
+typedef WeatherEmoji = String;
+Future<WeatherEmoji> getWeather(City city) {
+  return Future.delayed(
+    const Duration(seconds: 1),
+    () => {
+      City.stockholm: '❄️',
+      City.paris: '🌧️',
+      City.tokyo: '💨',
+    }[city]!,
+  );
+}
+
+// UI writes to and reads from this
+final currentCityProvider = StateProvider<City?>(
+  (ref) => null,
+);
+
+const unknownWeatherEmoji = '🤷‍♂️';
+
+// UI reads this
+final weatherProvider = FutureProvider<WeatherEmoji>((ref) {
+  final city = ref.watch(currentCityProvider);
+  if (city != null) {
+    return getWeather(city);
+  } else {
+    return unknownWeatherEmoji;
+  }
+});
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentWeather = ref.watch(
+      weatherProvider,
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather'),
+      ),
+      body: Column(
+        children: [
+          currentWeather.when(
+            data: (data) => Text(
+              data,
+              style: const TextStyle(fontSize: 40),
+            ),
+            error: (_, __) => const Text('Error 😥'),
+            loading: () => const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: CircularProgressIndicator(),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: City.values.length,
+              itemBuilder: (context, index) {
+                final city = City.values[index];
+                final isSelected = city == ref.watch(currentCityProvider);
+                return ListTile(
+                  title: Text(city.toString()),
+                  trailing: isSelected ? const Icon(Icons.check) : null,
+                  onTap: () => ref
+                      .read(
+                        currentCityProvider.notifier,
+                      )
+                      .state = city,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
